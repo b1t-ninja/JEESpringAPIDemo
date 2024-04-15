cb815eefc4ffc6b68028e58db19ca06ce5992997
ninja <ninja@nicos-mbp.localdomain>
2024-04-15 17:13:33 +0200

    initial commit

---
+++ src/main/java/com/example/demo/Payroll/Employee.java
@@ -0,0 +1,46 @@
+package com.example.demo.Payroll;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+// Dataobject used for persistent storage
+@Getter
+@Setter
+@ToString
+@Entity
+public class Employee {
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private String role;
+
+    public Employee() {}
+
+    Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Employee employee = (Employee) o;
+        return getId() != null && Objects.equals(getId(), employee.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
